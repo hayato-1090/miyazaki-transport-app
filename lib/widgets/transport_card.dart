e0f73298cf5f64ebfc48e8507ec16d0c916a4665
hayato-1090 <hajinzhanggu@gmail.com>
2026-03-15 09:27:28 +0900

    Add TransportCard widget

---
+++ lib/widgets/transport_card.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+class TransportCard extends StatelessWidget {
+  final IconData icon;
+  final String title;
+  final String description;
+
+  TransportCard({required this.icon, required this.title, required this.description});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 2,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Row(
+          children: [
+            Icon(icon, size: 40, color: Colors.grey),
+            SizedBox(width: 16),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    title,
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(height: 4),
+                  Text(
+                    description,
+                    style: TextStyle(
+                      color: Colors.grey[600],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
