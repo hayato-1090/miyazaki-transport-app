29c2304a47892f1e55397adcdb5324469dce2905
hayato-1090 <hajinzhanggu@gmail.com>
2026-03-15 09:25:18 +0900

    Create lib/main.dart with Flutter application entry point

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'home_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Miyazaki Transport App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        brightness: Brightness.light,
+      ),
+      home: HomeScreen(),
+      locale: Locale('ja'), // Japanese language
+    );
+  }
+}
